58a2d88403a15f8e568523b4d490e054094f1f9f
EAkdora <108339327+EAkdora@users.noreply.github.com>
2022-07-12 18:48:47 +0300

    Update Soru3.sql
    3-customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki 
    first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.

--- Soru3.sql
+++ Soru3.sql
@@ -1,2 +1,2 @@
 SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
-LEFT JOIN rental ON rental.customer_id = customer.customer_id;
+FULL JOIN rental ON rental.customer_id = customer.customer_id;
