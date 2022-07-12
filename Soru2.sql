91951765c3c50a736c23100484bcf6a17b06ed2c
EAkdora <108339327+EAkdora@users.noreply.github.com>
2022-07-12 18:49:26 +0300

    Update Soru2.sql
    2-customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki 
    first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.

--- Soru2.sql
+++ Soru2.sql
@@ -1,3 +1,3 @@
 
 SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment
-LEFT JOIN customer ON payment.customer_id = customer.customer_id;
+RIGHT JOIN customer ON payment.customer_id = customer.customer_id;
